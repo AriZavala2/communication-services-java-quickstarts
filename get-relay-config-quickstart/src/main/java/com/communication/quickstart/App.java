946451aac3e00a92113030148744fd178d4b7093
Ari Zavala <arzavala@microsoft.com>
2022-04-05 16:04:27 -0700

    Add sample with ttl parameter

--- get-relay-config-quickstart/src/main/java/com/communication/quickstart/App.java
+++ get-relay-config-quickstart/src/main/java/com/communication/quickstart/App.java
@@ -8,15 +8,19 @@ import com.azure.communication.networktraversal.models.*;
 import org.kurento.client.WebRtcEndpoint;
 import org.kurento.client.MediaPipeline;
 import org.kurento.client.KurentoClient;
-import java.util.List;
+
 import java.lang.reflect.*;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.List;
 
 public class App 
 {
     private static KurentoClient kurento;
 
     // You can find your connection string from your resource in the Azure portal
-    private static String connectionString = "https://<RESOURCE_NAME>.communication.azure.com/;accesskey=<ACCESS_KEY>";
+    private static String connectionString = "endpoint=https://smstestapp.communication.azure.com/;accesskey=RGrzrWY80oV4a1KgTHXJ3mJAC/8V7mubH26IjYjF+LXYVtedhw1DejYxPc9wkIPC0b0vtA7oXDsmGxME2N5Ccg==";
     private static CommunicationIdentityClient communicationIdentityClient = new CommunicationIdentityClientBuilder()
                     .connectionString(connectionString)
                     .buildClient();
@@ -37,6 +41,9 @@ public class App
 
         System.out.println("Getting a relay configuration passing a Route Type");
         getRelayConfigurationUsingRouteType();
+
+        System.out.println("Getting a relay configuration passing a Ttl");
+        getRelayConfigurationUsingTtl();
     }
 
     public static void getRelayConfiguration()
@@ -71,7 +78,9 @@ public class App
             CommunicationUserIdentifier user = communicationIdentityClient.createUser();
             System.out.println("User id: " + user.getId());
 
-            CommunicationRelayConfiguration config = communicationRelayClient.getRelayConfiguration(user);
+            GetRelayConfigurationOptions options = new GetRelayConfigurationOptions();
+            options.setCommunicationUserIdentifier(user);
+            CommunicationRelayConfiguration config = communicationRelayClient.getRelayConfiguration(options);
             
             System.out.println("Expires on:" + config.getExpiresOn());
             List<CommunicationIceServer> iceServers = config.getIceServers();
@@ -86,8 +95,30 @@ public class App
 
     public static void getRelayConfigurationUsingRouteType()
     {
-        CommunicationRelayConfiguration config = communicationRelayClient.getRelayConfiguration(RouteType.NEAREST);
+        GetRelayConfigurationOptions options = new GetRelayConfigurationOptions();
+        options.setRouteType(RouteType.ANY);
+        CommunicationRelayConfiguration config = communicationRelayClient.getRelayConfiguration(options);
         
+        System.out.println("Expires on:" + config.getExpiresOn());
+        List<CommunicationIceServer> iceServers = config.getIceServers();
+
+        for (CommunicationIceServer iceS : iceServers) {
+                System.out.println("URLS: " + iceS.getUrls());
+                System.out.println("Username: " + iceS.getUsername());
+                System.out.println("credential: " + iceS.getCredential());
+                System.out.println("RouteType: " + iceS.getRouteType());
+        }
+    }
+
+    public static void getRelayConfigurationUsingTtl()
+    {
+        GetRelayConfigurationOptions options = new GetRelayConfigurationOptions();
+        options.setTtl(60);
+        CommunicationRelayConfiguration config = communicationRelayClient.getRelayConfiguration(options);
+        
+        ZonedDateTime utc = ZonedDateTime.now(ZoneOffset.UTC);
+        System.out.println("Request time = " + utc.toInstant());
+
         System.out.println("Expires on:" + config.getExpiresOn());
         List<CommunicationIceServer> iceServers = config.getIceServers();
 
